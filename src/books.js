65e4e7d96d4176100eae2fa233b82877d419fa0e
vivek0200 <vivekkumarvk0217@gmail.com>
2023-11-16 13:59:20 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+
+export const books = [
+    {
+        author:'James Clear',
+        title: "Atomic Habits",
+        img: img1,
+        id:1,
+    },
+    {
+        author:'Francesc Miralles',
+        title: "Ikigai",
+        img: img2,
+        id:2,
+    },
+    {
+        author:'Joseph Murphy',
+        title: "The Power of your Subconscious Mind",
+        img: img3,
+        id:3,
+    }
+];
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { books } from "./books";
+import Book from "./Book";
+// const author='Victor E. Frankl';
+// const title="Man's Search For Meaning";
+// const img="./images/book1.jpg";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const BookList = () =>{
+    return (
+        <>
+            <h1>Amazon best sellers</h1>
+            <section className="booklist">     
+                {books.map((book,index)=>{
+                    return (
+                        <Book {...book}
+                        key={book.id} number={index} />
+                    );
+                })}
+                
+            </section>
+        </>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<BookList/>);
